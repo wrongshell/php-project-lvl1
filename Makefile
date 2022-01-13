139f4808d6f5291afbf13f25b074f0feddcbb310
wrongshell <wrongshell@imap.cc>
2022-01-13 14:54:59 +0300

    add step 7

--- Makefile
+++ Makefile
@@ -15,3 +15,6 @@ brain-even:
 
 brain-calc:
 	./bin/brain-calc
+
+brain-gcd:
+	./bin/brain-gcd
---
+++ src/types/Gcd.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Brain\Games\Types\Gcd;
+
+use Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function getDivisors(int $number): array
+{
+    for ($i = 1; $i <= $number; $i++) {
+        if (is_int($number / $i)) {
+            $divisors[] = $i;
+        }
+    }
+
+    return $divisors;
+}
+
+function generateTask(): array
+{
+    $number1 = random_int(1, 99);
+    $number2 = random_int(1, 99);
+    $divisors = array_intersect(getDivisors($number1), getDivisors($number2));
+
+    $task['question'] = "{$number1} {$number2}";
+    $task['answer'] = end($divisors);
+
+    return $task;
+}
+
+function playGcd(): bool
+{
+    $name = Engine\getName();
+    line('What is the result of the expression?');
+
+    $scores = 0;
+
+    while ($scores < Engine\MAX_WINS) {
+        $task = generateTask();
+        Engine\askQuestion($task['question']);
+        $userAnswer = Engine\getAnswer();
+        Engine\checkAnswer($name, $task['answer'], $userAnswer) ? $scores++ : $scores = 0;
+    }
+
+    Engine\printCongrats($name);
+
+    return true;
+}
