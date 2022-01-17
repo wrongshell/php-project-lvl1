9ddc81cc7da05e06557e363ff1ac48cc7addc6fb
wrongshell <wrongshell@imap.cc>
2022-01-17 16:55:42 +0300

    new structure

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,45 +2,26 @@
 
 namespace BrainGames\Games\Gcd;
 
-use BrainGames\Engine;
-
-use function cli\line;
-use function BrainGames\Engine\{getName, askQuestion, getAnswer, checkAnswer, printCongrats};
+use function BrainGames\Engine\playGame;
 use function BrainGames\Misc\getDivisors;
 
-use const BrainGames\Engine\MAX_WINS;
+const GAME = 'Gcd';
+const RULES = 'Find the greatest common divisor of given numbers.';
 
-function generateTask(): array
+function play(): void
 {
-    $task = array();
+    $generateTask = function (): array {
+        $number1 = random_int(1, 99);
+        $number2 = random_int(1, 99);
+        $divisors = array_intersect(getDivisors($number1), getDivisors($number2));
+        $task = [];
+        $task['question'] = "{$number1} {$number2}";
+        $task['answer'] = end($divisors);
 
-    $number1 = random_int(1, 99);
-    $number2 = random_int(1, 99);
-    $divisors = array_intersect(getDivisors($number1), getDivisors($number2));
+        return $task;
+    };
 
-    $task['question'] = "{$number1} {$number2}";
-    $task['answer'] = end($divisors);
+    playGame(GAME, RULES, $generateTask);
 
-    return $task;
-}
-
-function playGcd(): bool
-{
-    $name = getName();
-    line('Find the greatest common divisor of given numbers.');
-
-    for ($scores = 0; $scores < MAX_WINS;) {
-        $task = generateTask();
-        askQuestion($task['question']);
-        $userAnswer = getAnswer();
-        if (checkAnswer($name, $task['answer'], $userAnswer)) {
-            $scores++;
-        } else {
-            return false;
-        }
-    }
-
-    printCongrats($name);
-
-    return true;
+    return;
 }
