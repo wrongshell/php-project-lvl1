af4a64ec9a33849af02fd36f86c4442f743b47e3
wrongshell <wrongshell@imap.cc>
2022-01-17 17:12:04 +0300

    new structure

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,21 +7,6 @@ use function BrainGames\Engine\playGame;
 const GAME = 'Calc';
 const RULES = 'What is the result of the expression?';
 
-function generateTask(): array
-{
-    $task = array();
-
-    $operations = ['+','-','*'];
-    $operation = $operations[random_int(0, 2)];
-    $number1 = random_int(1, 99);
-    $number2 = random_int(1, 99);
-
-    $task['question'] = "{$number1} {$operation} {$number2}";
-    $task['answer'] = eval('return ' . $task['question'] . ';');
-
-    return $task;
-}
-
 function play(): void
 {
     $generateTask = function (): array {
