4a9bc781391e0c1f700fe873f3ae76df7a00478f
wrongshell <wrongshell@imap.cc>
2022-01-18 11:01:22 +0300

    fix: use array_rand in calc game

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -11,7 +11,8 @@ function play(): void
 {
     $generateTask = function (): array {
         $operations = ['+','-','*'];
-        $operation = $operations[random_int(0, 2)];
+        // $operation = $operations[random_int(0, count($operations) - 1)];
+        $operation = $operations[array_rand($operations)];
         $number1 = random_int(1, 99);
         $number2 = random_int(1, 99);
         $task = [];
