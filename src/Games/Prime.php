4add401f93d2a84022a40c24fb827354464c31a8
wrongshell <wrongshell@imap.cc>
2022-02-02 18:07:12 +0300

    fix: update prime game

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,15 +8,30 @@ use function BrainGames\Misc\getDivisors;
 const GAME_NAME = 'Prime';
 const GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+function isPrime(int $number): bool
+{
+    if ($number <= 1 || $number % 2 === 0 || $number % 3 === 0) {
+        return false;
+    } elseif ($number === 2 || $number === 3) {
+        return true;
+    } else {
+        for ($i = 5; $i < $number; $i++) {
+            if ($number % $i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 function play(): void
 {
     $generateTask = function (): array {
         $number = random_int(1, 99);
-        $isPrime = count(getDivisors($number)) === 2;
 
         return [
             'question' => $number,
-            'answer' => $isPrime ? 'yes' : 'no'
+            'answer' => isPrime($number) ? 'yes' : 'no'
         ];
     };
 
